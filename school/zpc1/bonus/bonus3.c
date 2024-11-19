1aad6c94e2f11f52adeef79c969810d40042481a
egirlcatnip <egirlcatnip@gmail.com>
2024-11-19 12:37:27 +0100

    Updates

--- school/zpc1/bonus/bonus3.c
+++ school/zpc1/bonus/bonus3.c
@@ -1,10 +1,98 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-int main(void) {
-  //
+int root(int n) {
+  int i = 1;
+  while (i * i < n) {
+    i++;
+  }
+  return i;
+}
 
-  printf("test\n");
+int get_matrix_by_index(int matrix[], int m, int index_x, int index_y) {
+  assert(!(index_x > m || index_y > m || index_x < 0 || index_y < 0));
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      int value = matrix[index_x * m + index_y];
+      assert(!(value != 0 && value != 1));
+    }
+  }
+
+  return matrix[index_x * m + index_y];
+}
+
+void check_matrix(int matrix[], int m) {
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      int value = get_matrix_by_index(matrix, m, i, j);
+      assert(value != -1);
+    }
+  }
+}
+
+void print_matrix(int matrix[], int m) {
+  check_matrix(matrix, m);
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      int value = get_matrix_by_index(matrix, m, i, j);
+
+      printf("%d  ", value);
+    }
+    printf("\n");
+  }
+}
+
+// Bonus functions:
+int equivalence(int r[], int m) {
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      int value_i = get_matrix_by_index(r, m, i, j);
+      int value_j = get_matrix_by_index(r, m, j, i);
+
+      if (value_i != value_j) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+void transitive_closure(int r[], int m) {
+  // Change stuff
+};
+
+int main(void) {
+  int matrix[] = {
+      0, 0, 0, 0, //
+      0, 0, 0, 0, //
+      0, 0, 1, 0, //
+      0, 0, 0, 1, //
+  };
+  int m = 4;
+
+  printf("Matrix:\n");
+  print_matrix(matrix, m);
+
+  printf("\n");
+
+  int equivalent = equivalence(matrix, m);
+
+  if (equivalent) {
+    printf("The matrix is equivalent.\n");
+  } else {
+    printf("The matrix is not equivalent.\n");
+  }
+
+  printf("\n");
+  printf("Transitive closure:\n");
+
+  transitive_closure(matrix, m);
+
+  print_matrix(matrix, m);
 
   return 0;
 }
@@ -13,11 +101,17 @@ int main(void) {
 
 // Expected output
 /*
-String: "ahoj svete ah"
-Looking for: "ahojte"
-ahoj <0,3>
-ah <11,12>
+Matrix:
+0  0  0  0
+0  0  0  0
+0  0  1  0
+0  0  0  1
 
-Found at index 0 with len 4
-ahoj <0,3>
+The matrix is equivalent.
+
+Transitive closure:
+0  0  0  0
+0  0  0  0
+0  0  1  0
+0  0  0  1
 */
