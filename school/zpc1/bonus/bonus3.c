05341979933efb79c302ca7158f0724a151010f0
egirlcatnip <egirlcatnip@gmail.com>
2024-11-19 13:04:40 +0100

    Updated

--- school/zpc1/bonus/bonus3.c
+++ school/zpc1/bonus/bonus3.c
@@ -2,38 +2,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-int root(int n) {
-  int i = 1;
-  while (i * i < n) {
-    i++;
-  }
-  return i;
-}
-
+// Helper functions
 int get_matrix_by_index(int matrix[], int m, int index_x, int index_y) {
-  assert(!(index_x > m || index_y > m || index_x < 0 || index_y < 0));
+  int valid = (index_x > m || index_y > m || index_x < 0 || index_y < 0);
+  assert(!valid);
 
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < m; j++) {
       int value = matrix[index_x * m + index_y];
-      assert(!(value != 0 && value != 1));
+      int valid = (value != 0 && value != 1);
+
+      assert(!valid);
     }
   }
-
   return matrix[index_x * m + index_y];
 }
 
-void check_matrix(int matrix[], int m) {
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < m; j++) {
-      int value = get_matrix_by_index(matrix, m, i, j);
-      assert(value != -1);
-    }
-  }
-}
-
 void print_matrix(int matrix[], int m) {
-  check_matrix(matrix, m);
 
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < m; j++) {
@@ -45,9 +30,8 @@ void print_matrix(int matrix[], int m) {
   }
 }
 
-// Bonus functions:
-int equivalence(int r[], int m) {
-
+// Checks
+int symetric(int r[], int m) {
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < m; j++) {
       int value_i = get_matrix_by_index(r, m, i, j);
@@ -61,23 +45,56 @@ int equivalence(int r[], int m) {
   return true;
 };
 
+int reflexive(int r[], int m) {
+  for (int i = 0; i < m; i++) {
+    int value = get_matrix_by_index(r, m, i, i);
+
+    if (value == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int transitive(int r[], int m) {
+  return 0;
+  //
+};
+
+// Exercise functions
+int equivalence(int r[], int m) {
+  int is_symetric = symetric(r, m);
+  int is_transitive = transitive(r, m);
+  int is_reflexive = reflexive(r, m);
+
+  if (is_symetric && is_transitive && is_reflexive) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 void transitive_closure(int r[], int m) {
   // Change stuff
+
 };
 
 int main(void) {
   int matrix[] = {
-      0, 0, 0, 0, //
-      0, 0, 0, 0, //
-      0, 0, 1, 0, //
-      0, 0, 0, 1, //
+      1, 0, 0, //
+      0, 1, 0, //
+      0, 0, 1, //
   };
-  int m = 4;
+  int m = 3;
 
   printf("Matrix:\n");
   print_matrix(matrix, m);
 
   printf("\n");
+
+  printf("Is reflexive?: %d \n", reflexive(matrix, m));
+  printf("Is symteric?: %d \n", symetric(matrix, m));
+  printf("Is transitive?: %d \n", transitive(matrix, m));
 
   int equivalent = equivalence(matrix, m);
 
@@ -102,16 +119,16 @@ int main(void) {
 // Expected output
 /*
 Matrix:
-0  0  0  0
-0  0  0  0
+1  0  0  0
+1  1  0  0
 0  0  1  0
 0  0  0  1
 
-The matrix is equivalent.
+The matrix is not equivalent.
 
 Transitive closure:
-0  0  0  0
-0  0  0  0
+1  1  0  0
+1  1  0  0
 0  0  1  0
 0  0  0  1
 */
